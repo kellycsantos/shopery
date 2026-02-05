d74fe79d835f5490d32798d16e2cd8edd98fc2e5
Kelly C Santos <kellycristinasntos@gmail.com>
2026-02-05 20:47:21 -0300

    feat: created checkbox components

---
+++ src/app/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,23 @@
+'use client'
+import styles from './checkbox.module.scss'
+import { Check } from 'lucide-react'
+
+type CheckboxProps = {
+    label: string,
+    value: string,
+    onClick?: (e:any) => void,
+    onChange?: (e: any) => void,
+}
+export const Checkbox = ({label, value, onClick, onChange}: CheckboxProps) => {
+    return (
+        <>
+            <label htmlFor={value} className={styles.checkbox_container}>
+                <span className={styles.box}>
+                    <Check color='#FFFFFF' strokeWidth={3}/>
+                </span>
+                <input type="checkbox" id={value} value={value} onClick={(e) => onClick?.(e)} onChange={(e) => onChange?.(e.target.checked)} />
+                {label}
+            </label>
+        </>
+    )
+}
---
+++ src/app/components/Checkbox/checkbox.spec.tsx
@@ -0,0 +1,10 @@
+import {render, screen} from '@testing-library/react';
+import {it, vi, describe, expect} from 'vitest';
+import { Checkbox } from './Checkbox';
+
+describe('Checkbox', () => { 
+    it('Deve renderizar o componente corretamente', () => {
+        render(<Checkbox label='I agree' value='true'/>)
+        expect(screen.getByTestId('checkbox')).toBeInTheDocument()
+    })
+ })
