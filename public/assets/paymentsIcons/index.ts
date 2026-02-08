c7e46bb7235a68d87b249636672049345e5a57f5
Kelly C Santos <kellycristinasntos@gmail.com>
2026-02-07 22:38:18 -0300

    styles: fix layout topbar, main and export icons

---
+++ public/assets/paymentsIcons/index.ts
@@ -0,0 +1,7 @@
+import applePay from './apple-pay.svg'
+import discover from './discover.svg'
+import mastercard from './mastercard.svg'
+import securePayment from './secure-payment.svg'
+import visa from './visa.svg'
+
+export default {applePay, discover, mastercard, securePayment, visa};
