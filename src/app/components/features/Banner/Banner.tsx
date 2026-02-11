71817e908d64e233eb11ac5246ae374fd55edc22
Kelly C Santos <kellycristinasntos@gmail.com>
2026-02-10 23:52:40 -0300

    style: add limit to banner size

--- src/app/components/features/Banner/Banner.tsx
+++ src/app/components/features/Banner/Banner.tsx
@@ -3,20 +3,23 @@ import Image from 'next/image';
 
 import bannerImg from '@/assets/banner_image.png'
 
-import {Button} from '@/components'
+import { Button } from '@/components'
 
 export const Banner = () => {
     return (
         <div className={style.banner}>
-            <Image src={bannerImg} alt='fruit banner' />
-            <section className={style.info}>
-                <p></p>
-                <h1>Fresh & Healthy <br />
-                    Organic Food
-                </h1>
-                <p>Free shipping on all your order. we deliver, you enjoy</p>
-                <Button text='Shop now'/>
-            </section>
+            <div className={style.limit}>
+
+                <Image src={bannerImg} alt='fruit banner' />
+                <section className={style.info}>
+                    <p></p>
+                    <h1>Fresh & Healthy <br />
+                        Organic Food
+                    </h1>
+                    <p>Free shipping on all your order. we deliver, you enjoy</p>
+                    <Button text='Shop now' size='lg' />
+                </section>
+            </div>
         </div>
     )
 }
