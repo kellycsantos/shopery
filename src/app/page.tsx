fffae3e7441bcbecc8a1522b60d3ace0f4bea85a
Kelly C Santos <kellycristinasntos@gmail.com>
2026-02-10 23:53:52 -0300

    feat: reset initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1>Shopery !</h1>
-        <h2>Titulo 2</h2>
-        <h3>Titulo 3</h3>
-        <h4>Titulo 4</h4>
-        <h5>Titulo 5</h5>
-        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quia quisquam eligendi tempora quam voluptatum ipsa voluptate aspernatur dignissimos? Aliquid!</p>
-      <Input
-      placeholder='Email'
-      />
-
-      <Link href={'/register'}>Registrar</Link><br/>
-      <Link href={'/login'}>Login</Link>
-      <Banner/>
+        <h1>Home page</h1>
       </main>
     </div>
   );
