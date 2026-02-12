983f6d9c0a2d142ddb854873e8677d06551c7427
Kelly C Santos <kellycristinasntos@gmail.com>
2026-02-11 21:33:29 -0300

    feat: add dynamic info and style finished

--- src/app/components/features/Extras/Extras.tsx
+++ src/app/components/features/Extras/Extras.tsx
@@ -1,13 +1,28 @@
-import { Headset } from "lucide-react"
 import style from './extra.module.scss'
-export const Extras = () => {
-    return(
-        <section className={style.extra_container}>
-            <article>
-                <Headset/>
-                <h6>Free Shipping</h6>
-                <small>Free shipping with discount</small>
-            </article>
+
+import { Truck, Headset, ShieldCheck, Package } from 'lucide-react'
+
+export const Extras = ({isOverlap = false}) => {
+    const features = [
+        { title: 'Free Shipping', subtitle: 'Free shipping with discount', svg: <Truck /> },
+        { title: 'Great Support 24/7', subtitle: 'Instant access to Contact', svg: <Headset /> },
+        { title: '100% Sucure Payment', subtitle: 'We ensure your money is save', svg: <ShieldCheck /> },
+        { title: 'Money-Back Guarantee', subtitle: '30 days money-back guarantee', svg: <Package /> },
+    ]
+    return (
+        <section className={`${style.extra_container} ${isOverlap && style.overlap }`}>
+            {
+                features.map((feature, index) => (
+                    <article key={index}>
+                        <span className={style.icon_container}>
+                            {feature.svg}
+
+                        </span>
+                        <h6>{feature.title}</h6>
+                        <small>{feature.subtitle}</small>
+                    </article>
+                ))
+            }
         </section>
     )
 }
