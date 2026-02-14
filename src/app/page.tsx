b7ea7ea9c456b137ead834f91b2a355084f49506
Kelly C Santos <kellycristinasntos@gmail.com>
2026-02-14 18:19:31 -0300

    style: adjust mobile components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,11 @@ export default function Home() {
       <Banner />
       <Extras isOverlap />
       <main className={style.main}>
+        <div className={style.introducing}>
+          <div className={style.limit}>
+            <h3>Introducing Our Products</h3>
+          </div>
+        </div>
 
       </main>
       <Footer />
