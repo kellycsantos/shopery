4363cd1c9ba8745a55258c77872c2b2e0c5361b0
Kelly C Santos <kellycristinasntos@gmail.com>
2026-02-17 23:59:06 -0300

    feat: call api on products page

--- src/app/(shopping)/products/page.tsx
+++ src/app/(shopping)/products/page.tsx
@@ -1,6 +1,18 @@
+'use client'
+import { getAllProducts } from "@/app/services/api";
+import { resolve } from "path";
+import { useState, useEffect } from "react";
+
+
 const Products = () => {
+    useEffect(() => {
+        getAllProducts().then((response) => {
+            console.log(response.data)
+        })
+    },[])
     return(
         <h1>Products Page</h1>
+
     )
 }
 
